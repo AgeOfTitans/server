c78f0cf303b6db29842134ec19f093b4909fb244
Mitch Freeman <65987027+neckkola@users.noreply.github.com>
2024-09-10 21:21:29 -0300

    Add strings and logic for displaying the evolving item xp transfer window in Corathus

--- zone/client.h
+++ zone/client.h
@@ -1819,6 +1819,7 @@ public:
 	void DoEvolveItemToggle(const EQApplicationPacket* app);
 	void DoEvolveItemDisplayFinalResult(const EQApplicationPacket* app);
 	bool DoEvolveCheckProgression(const EQ::ItemInstance &inst);
+	void SendEvolveXPTransferWindow();
 
 protected:
 	friend class Mob;
